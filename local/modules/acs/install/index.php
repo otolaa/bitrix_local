1b9d146b2a0f7303db7fb1444c534ad7cd54fa01
kalabunga82 <kalabunga82@gmail.com>
2021-08-27 22:28:15 +0200

    🍉

--- local/modules/acs/install/index.php
+++ local/modules/acs/install/index.php
@@ -14,18 +14,12 @@ Class acs extends CModule
     var $MODULE_DESCRIPTION;
     var $MODULE_CSS;
 
-    function acs()
+    public function __construct()
     {
         $arModuleVersion = [];
-
-        $path = str_replace("\\", "/", __FILE__);
-        $path = substr($path, 0, strlen($path) - strlen("/index.php"));
-        include($path."/version.php");
-        if (is_array($arModuleVersion) && array_key_exists("VERSION", $arModuleVersion))
-        {
-            $this->MODULE_VERSION = $arModuleVersion["VERSION"];
-            $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
-        }
+        include(__DIR__.'/version.php');
+        $this->MODULE_VERSION = $arModuleVersion["VERSION"];
+        $this->MODULE_VERSION_DATE = $arModuleVersion["VERSION_DATE"];
         $this->MODULE_NAME = GetMessage("acs_module_name");
         $this->MODULE_DESCRIPTION = GetMessage("acs_module_desc");
     }
